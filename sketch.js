8692d7e2ed301749a02413bf6971277bfc583b6a
YENNI27 <juliahu27@gmail.com>
2025-06-02 09:54:20 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,9 +31,6 @@ function draw() {
   background(220);
 
   // 顯示攝影機影像
-  video = createCapture(VIDEO);
-  video.size(width, height);
-  video.hide();
   image(video, 0, 0, width, height);
 
   // 畫出目標
@@ -74,10 +71,6 @@ function checkCollision() {
     const indexFinger = hand.landmarks[8]; // 食指尖端
     const d = dist(indexFinger[0], indexFinger[1], targetX, targetY);
     return d < 30; // 如果距離小於目標半徑，則視為碰撞
-  }
-  return false;
-}
-
   }
   return false;
 }
